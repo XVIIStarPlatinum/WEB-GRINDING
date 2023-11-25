634f987fe2ebd32185f29b92b2b4238f78ec6879
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2023-11-26 02:18:27 +0300

    Day1: 5/7 completed;

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     const todos = []
-    // [{desc: "learn js", "timestamp": new Date(), done: false}]
-
     const $form = document.querySelector("form")
     const $list = document.querySelector("ul")
 
-    const validate = () => {
-
+    const validate = (value) => {
+        if (!value || 3 < value.length > 51) {
+            alert("Invalid input.")
+            return false
+        }
+        return true
     }
 
 
     const renderTodos = () => {
-
+        $list.innerHTML = ""
+        for(let i = 0; i < todos.length; i++){
+            const item = todos[i]
+            const $li= document.createElement('li')
+            $li.innerText = item.desc
+            $list.appendChild($li)
+        }
     }
 
 
     $form.addEventListener("submit", (e) => {
         e.preventDefault()
-        // validate
-        // push new item into toos array
-        // re render arrays
-        // experiment with string interpolation to set the new content of the list
-        // clear the form
+        const $input = $form.querySelector("input")
+        const value = $input.value
+        if(!validate(value)) return false
+        todos.push({desc: value})
+        renderTodos()
+        $input.value = ""
         return false
     })
-
-
-
-})
+});
